fa296d407e53decf33f992475ee4d02b2c463955
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-08-24 01:12:25 +1000

    got response from bandsintown API

--- liri.js
+++ liri.js
@@ -2,4 +2,33 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+
+var artist = process.argv[2];
+
+axios
+    .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+        // If the axios was successful...
+        // Then log the body from the site!
+        console.log("Name of Venue: " + response.data[0].venue.name);
+    })
+    .catch(function (error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
