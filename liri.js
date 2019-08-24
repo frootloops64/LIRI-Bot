5f6f40366583fe2d942b6b4f209dd46d7e3bae20
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-08-25 01:28:43 +1000

    corrected some spelling errors

--- liri.js
+++ liri.js
@@ -62,13 +62,14 @@ function concerts() {
         .then(function (response) {
             var venueName = response.data[0].venue.name;
             var venueLocation = response.data[0].venue.city + ", " + response.data[0].venue.country;
-            var date = moment(reponse.data[0].datetime).format("MM/DD/YYYY");
+            var date = moment(response.data[0].datetime).format("MM/DD/YYYY");
             var output = divider +
                 "\nVenue: " + venueName +
                 "\nVenue Location: " + venueLocation +
                 "\nDate: " + date +
                 divider;
 
+            console.log("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp");
             console.log(output);
         })
         .catch(function (error) {
@@ -99,11 +100,11 @@ function movies() {
 
         console.log(output);
     }
-    var movie = Array.prototype.join.call(searchTerm, "+");
+    
     axios
-        .get("http://omdbapi.com/?apikey=trilogy&t=" + movie)
+        .get("http://omdbapi.com/?apikey=trilogy&t=" + searchTerm)
         .then(function (response) {
-            var title = response.Title;
+            var title = response.data.Title;
             var year = response.data.Year;
             var rating = response.data.imdbRating;
             // var rottenTomatoes = JSON.stringify(response.data.Ratings[1]);
@@ -122,7 +123,27 @@ function movies() {
                 "\nPlot: " + plot +
                 "\nActors: " + actors +
                 divider;
-
+            console.log("http://omdbapi.com/?apikey=trilogy&t=" + movie);
             console.log(output);
         })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }
